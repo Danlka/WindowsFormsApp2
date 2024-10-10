59482bb72e10a87c276246c875702cd29c2dac4a
amax <mr.amax2000@mail.ru>
2024-10-10 16:10:57 +0300

    Initial commit

---
+++ BuilderBurger/IBurgerBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp2.DBCon;
+
+namespace WindowsFormsApp2.BuilderBurger
+{
+    public interface IBurgerBuilder
+    {
+        IBurgerBuilder AddCheese();
+        IBurgerBuilder AddBacon();
+        IBurgerBuilder AddLetuce();
+        IBurgerBuilder AddTomato();
+        IBurgerBuilder AddPickles();
+        Burgers GetBurgers();
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp2.BuilderBurger;
+using WindowsFormsApp2.DBCon;
+
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Model1 Model = new Model1();
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BurgerBuilder burgerBuilder = new BurgerBuilder();
+            BurgerDirector burgerDirector = new BurgerDirector(burgerBuilder);
+
+            if (comboBox1.SelectedItem.ToString() == "Бургер стандартный")
+                burgerDirector.BuildDefault();
+            else
+                burgerDirector.BuildWithBeacon();
+
+            try
+            {
+                Model.Burgers.Add(burgerBuilder.GetBurgers());
+                Model.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            comboBox1.SelectedIndex = 0;
+            dataGridView1.DataSource = Model.Burgers.ToList();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
